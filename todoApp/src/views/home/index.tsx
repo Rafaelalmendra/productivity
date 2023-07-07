564af9fab913b04d3223bbe36ab91e49828bf2c7
Rafael Almendra <rafaelalmendra28@gmail.com>
2023-07-07 04:13:12 -0300

    feat: add user name in todo app

--- todoApp/src/views/home/index.tsx
+++ todoApp/src/views/home/index.tsx
@@ -3,13 +3,21 @@ import React from "react";
 // components
 import { Layout } from "host/Layout";
 
+// contexts
+import { useAuth } from "host/useAuth";
+
 const HomeView = () => {
+  const { user } = useAuth();
+
   return (
     <Layout>
-      <div className="mt-4">
-        <h1 className="font-bold text-gray-600 text-3xl text-gray-800">
-          Lista de tarefas
-        </h1>
+      <div className="mt-8">
+        <div className="flex flex-col gap-2">
+          <h1 className="font-bold text-3xl text-gray-800">Lista de tarefas</h1>
+          <p className="text-gray-700">
+            Bem vindo, <span className="font-bold">{user?.name}</span>
+          </p>
+        </div>
       </div>
     </Layout>
   );
